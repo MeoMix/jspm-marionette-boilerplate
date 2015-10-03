613dbde45023a7833938d7c880212178871e6f3b
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2015-10-02 21:51:19 -0700

    Add IE and FireFox to Karma

--- karma.conf.js
+++ karma.conf.js
@@ -54,7 +54,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'Firefox', 'IE'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
@@ -66,6 +66,10 @@ module.exports = function(config) {
       Chrome_travis_ci: {
         base: 'Chrome',
         flags: ['--no-sandbox']
+      },
+      ChromeCanary_travis_ci: {
+        base: 'ChromeCanary',
+        flags: ['--no-sandbox']
       }
     }
   };
@@ -74,7 +78,7 @@ module.exports = function(config) {
   // Need to pass the --no-sandbox flag in to use as expected.
   // http://stackoverflow.com/a/25661593/633438
   if (process.env.TRAVIS) {
-    configuration.browsers = ['Chrome_travis_ci'];
+    configuration.browsers = ['Chrome_travis_ci', 'Firefox', 'IE'];
   }
 
   config.set(configuration);
