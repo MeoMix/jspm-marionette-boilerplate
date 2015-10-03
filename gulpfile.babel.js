aacf3a0b9ac20edae07a322b95c072ff37467952
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2015-10-02 18:07:11 -0700

    Finish compile build task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,26 +4,39 @@ const babel = require('gulp-babel');
 const plumber = require('gulp-plumber');
 const eslint = require('gulp-eslint');
 const gutil = require('gulp-util');
+const copy = require('gulp-copy');
 const Builder = require('systemjs-builder');
 const Server = require('karma').Server;
+const srcFiles = ['src/**/*'];
 const srcJavaScript = ['src/**/*.js'];
+const srcNonJavaScript = ['src/**/*', '!src/**/*.js'];
 const allJavaScript = ['src/**/*.js', 'test/**/*.js', 'gulpfile.babel.js'];
+
+// Move files which do not need to be compiled (e.g. .css, .html, .hbs)
+// to the /compiled directory.
+gulp.task('copy-srcNonJavaScript', () => {
+  return gulp.src(srcNonJavaScript)
+    .pipe(plumber())
+    .pipe(copy('compiled', { prefix: 1 }));
+});
 
 // Transpile ES6 files and copy results to /compiled directory.
 // Copy non-ES6 files to /compiled directory.
-gulp.task('compile', () => {
-  return gulp.
-    src(srcJavaScript).
-    pipe(plumber()).
-    pipe(babel({ modules: 'system' })).
-    pipe(gulp.dest('compiled'));
+gulp.task('compile', ['copy-srcNonJavaScript'], () => {
+  return gulp.src(srcJavaScript)
+    .pipe(plumber())
+    .pipe(babel({
+      modules: 'system'
+    }))
+    .pipe(gulp.dest('compiled'));
 });
 
 // Watch source files for changes. Run compile task when changes detected.
 gulp.task('watch', () => {
-  return gulp.
-    watch(srcJavaScript, ['compile']).
-    on('change', (event) => {
+  const watchTasks = ['compile'];
+
+  return gulp.watch(srcFiles, watchTasks)
+    .on('change', (event) => {
       const simplePath = event.path.replace(__dirname, '');
       gutil.log(gutil.colors.cyan(`${simplePath} was ${event.type}, recompiling...`));
     });
@@ -40,21 +53,23 @@ gulp.task('build', (done) => {
     minify: false
   };
 
-  builder.buildStatic('src/main.js', `${dest}main.js`, options).then(() => {
-    gutil.log(gutil.colors.green(`Built successfully to ${dest}`));
-  }).catch((errorMessage) => {
-    gutil.log(gutil.colors.red(errorMessage));
-  }).finally(done);
+  builder.buildStatic('src/main.js', `${dest}main.js`, options)
+    .then(() => {
+      gutil.log(gutil.colors.green(`Built successfully to ${dest}`));
+    })
+    .catch((errorMessage) => {
+      gutil.log(gutil.colors.red(errorMessage));
+    })
+    .finally(done);
 });
 
 // Lint and enforce code quality style. Configuration found in .jscsrc files.
 // http://jscs.info/
 gulp.task('lint', () => {
-  return gulp.
-    src(allJavaScript).
-    pipe(eslint()).
-    pipe(eslint.format()).
-    pipe(eslint.failAfterError());
+  return gulp.src(allJavaScript)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
 // Start a Karma server and execute test suites. Configuraiton bound in karma.conf.js
