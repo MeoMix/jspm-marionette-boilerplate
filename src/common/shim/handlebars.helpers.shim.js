7b8cd650491f68bda0380c1ab8f9632de97aa006
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2015-10-08 16:38:47 -0700

    Add shims

---
+++ src/common/shim/handlebars.helpers.shim.js
@@ -0,0 +1,10 @@
+﻿import Handlebars from 'handlebars';
+
+// Provide handlebars with the ability to look up a model's attributes from within a template.
+Handlebars.registerHelper('get', (model, attribute) => {
+  return model.get(attribute);
+});
+
+Handlebars.registerHelper('ternary', (test, yes, no) => {
+  return test ? yes : no;
+});
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-﻿import Application from './application/application';
+﻿import './common/shim/handlebars.helpers.shim';
+import './common/shim/backbone.marionette.region.shim';
+import './common/shim/backbone.marionette.toJson.shim';
+import './common/shim/backbone.marionette.view.shim';
+import Application from './application/application';
 
 const application = new Application();
 application.start();
